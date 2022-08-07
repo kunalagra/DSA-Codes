364ac0cde319ed258e6e00c58916a83eb0c5101a
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2022-08-07 22:37:36 +0530

    update DFS for user-input graph

--- Algorithms/Depth First Search.py
+++ Algorithms/Depth First Search.py
@@ -1,23 +1,25 @@
-graph = {
-	'A': ['B', 'D'],
-	'B': ['C', 'F'],
-	'C': ['E', 'G', 'H'],
-	'D': ['F'],
-	'E': ['B', 'F'],
-	'F': ['A'],
-	'G': ['E', 'H'],
-	'H': ['A'],
-}
-visited = []
+# Dictionary used for the graph
+graph = {}
 
+# Keep unvisisted node
+visited = set()
 
 def dfs(visited, graph, node):
-	if node not in visited:
-		print(node, end=" ")
-		visited.append(node)
-		for neighbour in graph[node]:
-			dfs(visited, graph, neighbour)
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
 
+# Driver Code
+numnode = int(input("No. Of Nodes in Graph: "))
+while numnode > 0:
+    key = input("Enter Key: ")
+    value = input("Enter Values: ").split(" ")
+    if value == [""]:
+        value = []
+    graph[key] = value
+    numnode -= 1
 
 root = input("Enter the Root Node: ")
 print("\nFollowing is the Depth-First Search Result: ")
